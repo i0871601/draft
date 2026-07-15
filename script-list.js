4194f4a7b45565672026f75ec215ea478ff3697b
i0871601 <i0871601@gmail.com>
2026-07-15 18:21:43 +0300

    0.00.003

--- script-list.js
+++ script-list.js
@@ -142,9 +142,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Якщо закладки мають бути видимі, знімаємо "checked" з керуючих чекбоксів
     if (buttonVisibility) {
-        console.log("П")
         buttonVisibility.forEach(el => {
-            console.log("G")
             el.checked = false; 
         });
     }
