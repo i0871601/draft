2d88421382f58f5a93017c043b2713224add0f7f
i0871601 <i0871601@gmail.com>
2026-09-01 19:34:07 +0300

    0.00.005

--- script-list-lesson.js
+++ script-list-lesson.js
@@ -27,67 +27,68 @@ export const listLessonDay = (dayText) => {
 
     if (checkboxVisibility.checked) {
         checkboxVisibility.checked = false;
-        setTimeout(() => {
-            contentRoutine.innerHTML = '';
-        }, 500);
     }
+    setTimeout(() => {
+        contentRoutine.innerHTML = '';
 
-    if (filteredLessons && filteredLessons.length > 0) {
-        filteredLessons.forEach(el =>{ 
-            const [startTime] = el.Time ? el.Time.split('-') : ['', ''];
-
-            const activeId = `active-lesson-${el.LessonNumber}`;
-            const passedId = `passed-lesson-${el.LessonNumber}`;
-
-            let locationHTML = '';
-            if (el.Link) {
-                const rawLink = el.Link.trim();
-                const isUrl = rawLink.startsWith('http://') || rawLink.startsWith('https://');
+        if (filteredLessons && filteredLessons.length > 0) {
+            filteredLessons.forEach(el => {
+                const [startTime] = el.Time ? el.Time.split('-') : ['', ''];
                 
-                if (isUrl) locationHTML = `
-                    <a href="${rawLink}" target="_blank" class="lesson-location link">посилання</a>
-                `;
-
-                else locationHTML = `
-                    <p class="lesson-location text">${rawLink}</p>
-                `;
-            }
-            
-            const lessonHTML = `
-            <article class="routine-entry">
-                <input type="checkbox" id="${activeId}" class="input input-active">
-                <input type="checkbox" id="${passedId}" class="input input-passed">
+                const activeId = `active-lesson-${el.LessonNumber}`;
+                const passedId = `passed-lesson-${el.LessonNumber}`;
                 
-                <label for="${activeId}" class="lesson-label">
-                    <div class="icon">
-                        <span class="status-icon"></span>
-                    </div>
-
-                    <div class="info-lesson">
-                        <div class="name-subject-class">
-                            <p class="name-subject">${el.Subject}</p>
-                            <p class="name-class">${el.Class}</p>
-                        </div>
+                let locationHTML = '';
+                
+                if (el.Link) {
+                    const rawLink = el.Link.trim();
+                    const isUrl = rawLink.startsWith('http://') || rawLink.startsWith('https://');
+                    
+                    if (isUrl) locationHTML = `
+                        <a href="${rawLink}" target="_blank" class="lesson-location link">посилання</a>
+                    `;
+                    
+                    else locationHTML = `
+                        <p class="lesson-location text">${rawLink}</p>
+                    `;
+                }
+                
+                const lessonHTML = `
+                    <article class="routine-entry">
+                        <input type="checkbox" id="${activeId}" class="input input-active">
+                        <input type="checkbox" id="${passedId}" class="input input-passed">
                         
-                        ${locationHTML}
-                    </div>
-
-                    <div class="start-time-lesson">
-                        <h1>${startTime}</h1>
-                    </div>
-                </label>
-            </article>
+                        <label for="${activeId}" class="lesson-label">
+                            <div class="icon">
+                                <span class="status-icon"></span>
+                            </div>
+                            
+                            <div class="info-lesson">
+                                <div class="name-subject-class">
+                                    <p class="name-subject">${el.Subject}</p>
+                                    <p class="name-class">${el.Class}</p>
+                                </div>
+                                
+                                ${locationHTML}
+                            </div>
+                            
+                            <div class="start-time-lesson">
+                                <h1>${startTime}</h1>
+                            </div>
+                        </label>
+                    </article>
+                `;
+                
+                contentRoutine.insertAdjacentHTML('beforeend', lessonHTML);
+            });
+        } else {
+            contentRoutine.innerHTML = `
+                <div class="holiday">
+                    <p>Вихідний</p>
+                </div>
             `;
-            
-            contentRoutine.insertAdjacentHTML('beforeend', lessonHTML);
-        });
-    } else {
-        contentRoutine.innerHTML = `
-        <div class="holiday">
-            <p>Вихідний</p>
-        </div>
-        `;
-    }
+        };
 
-    if (!checkboxVisibility.checked) checkboxVisibility.checked = true;
+        if (!checkboxVisibility.checked) checkboxVisibility.checked = true;
+    }, 500);
 };
