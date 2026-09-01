aa67fbb7efb13909b42b1c9c8e12e46b163f2683
i0871601 <i0871601@gmail.com>
2026-09-01 17:21:46 +0300

    0.00.002

--- script-list-lesson.js
+++ script-list-lesson.js
@@ -1,4 +1,24 @@
 // Авторське право (c) вересень 2026 рік Сікан Іван Валерійович
+import { getUserData } from './config.js';
+
+const userData = getUserData();
+
+let routine = [];
+
+if (userData && userData.data.routine) {
+    routine = userData.data.routine;
+    console.log("Ось ваш масив routine:", routine);
+}
+
 export const listLessonDay = (dayName) => {
+
+    if (!Array.isArray(routine) || routine.length === 0) {
+        return [];
+    }
+
+    const filteredLessons = routine.filter(item => item.Day === dayText);
+    filteredLessons.sort((a, b) => Number(a.LessonNumber) - Number(b.LessonNumber));
+
+    console.log(filteredLessons);
 
 };
