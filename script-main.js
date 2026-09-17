66036dcc67d6dbff0d61129acad71be34436a915
i0871601 <i0871601@gmail.com>
2026-09-17 13:54:52 +0300

    Клік на кнопку розклад, активує скрипт

--- script-main.js
+++ script-main.js
@@ -3,11 +3,13 @@ import { calendar } from './script-calendar.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const inputTime = document.getElementById('time');
     const inputSelectFolder = document.getElementById('select-folder');
     const checkboxEventDay = document.getElementById('checkbox-event-day');
 
     document.querySelectorAll('input[name="trigger"]').forEach(radio => {
         radio.addEventListener('change', () => {
+            if (inputTime.checked) calendar();
             if (inputSelectFolder.checked) checkboxEventDay.checked = false;
         });
     });
