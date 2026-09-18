22faeb882600d8f4b6dfd7bcacb7e8302f04c996
i0871601 <i0871601@gmail.com>
2026-09-18 12:31:33 +0300

    Перевірка скрипта уроків 2

--- script-list-lesson.js
+++ script-list-lesson.js
@@ -113,6 +113,7 @@ function listLessonDay(dayText, isToday, eventDayContent) {
     filteredLessons.sort((a, b) => Number(a.LessonNumber) - Number(b.LessonNumber));
 
     console.log(filteredLessons);
+    alert(JSON.stringify(filteredLessons, null, 2));
 
     if(lessonUpdateTime) {
         clearTimeout(lessonUpdateTime);
